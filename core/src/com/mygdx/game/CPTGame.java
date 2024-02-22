a1bdc57be4e58cef4d2fc55e7aab070db10a1737
matthewgorman <mgormangamer@gmail.com>
2024-02-22 10:44:26 -0500

    optimized code

--- core/src/com/mygdx/game/CPTGame.java
+++ core/src/com/mygdx/game/CPTGame.java
@@ -12,7 +12,7 @@ public class CPTGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("among us.png");
 	}
 
 	@Override
