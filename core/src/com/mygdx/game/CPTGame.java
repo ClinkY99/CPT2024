d2ba5fcf1d44cd0b410bf20812282dcbccc18e69
matthewgorman <mgormangamer@gmail.com>
2024-02-26 13:27:59 -0500

    optimized code

--- core/src/com/mygdx/game/CPTGame.java
+++ core/src/com/mygdx/game/CPTGame.java
@@ -1,6 +1,8 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -8,11 +10,16 @@ import com.badlogic.gdx.utils.ScreenUtils;
 public class CPTGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+	private Texture bucketImage;
+	private Sound susSound;
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("among us.png");
+		// declares images
+		bucketImage = new Texture(Gdx.files.internal("bucketPicture.jpeg"));
+		susSound = Gdx.audio.newSound(Gdx.files.internal("among-us-start-sound.mp3"));
+		susSound.play();
 	}
 
 	@Override
--- desktop/src/com/mygdx/game/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
-		config.setWindowedMode(400, 800);
+		config.setWindowedMode(600, 800);
 		config.setTitle("CPT 2024");
 		new Lwjgl3Application(new CPTGame(), config);
 		System.out.println("Hello World!!!");
