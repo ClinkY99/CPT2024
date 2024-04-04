90e18014a80a17e944d9a2318486e1f08a347276
Clink_Y99 <clinekieran@gmail.com>
2024-04-04 14:32:51 -0400

    Added Menu Buttons

--- core/src/com/mygdx/game/Menus/Interactive/MenuButton.java
+++ core/src/com/mygdx/game/Menus/Interactive/MenuButton.java
@@ -1,9 +1,54 @@
 package com.mygdx.game.Menus.Interactive;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.Align;
+
 import java.awt.*;
 
-public class MenuButton {
-    public MenuButton(){
+public class MenuButton extends Button {
+    private Label label;
+    private FreeTypeFontGenerator generator;
 
+    private FreeTypeFontGenerator.FreeTypeFontParameter parameter;
+
+    public MenuButton(String Text){
+
+        this(Text, -1);
     }
+    public MenuButton(String Text, float scale){
+        super();
+        generator = new FreeTypeFontGenerator(Gdx.files.internal("MainMenu/tempus_sans_itc.ttf"));
+        parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+
+        parameter.size = 55;
+        if(scale != -1) parameter.size = 75;
+
+        parameter.color = Color.BLACK;
+
+        BitmapFont font = generator.generateFont(parameter);
+
+        setStyle(new ButtonStyle());
+        label = new Label(Text, new LabelStyle(font, null));
+        label.setAlignment(Align.center);
+
+        add(label).expand().fill();
+        setSize(getPrefWidth(),getPrefHeight());
+    }
+
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
+    }
+
+
+
 }
--- core/src/com/mygdx/game/Menus/MainMenu.java
+++ core/src/com/mygdx/game/Menus/MainMenu.java
@@ -6,30 +6,98 @@ import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.mygdx.game.CPTGame;
+import com.mygdx.game.Menus.Interactive.MenuButton;
+
+import java.util.Set;
 
 public class MainMenu implements Screen {
 
     final CPTGame game;
     OrthographicCamera camera;
-    BitmapFont MenuFont;
     Music MainMenuMusic;
     Texture MenuBackground;
+    Stage stage;
+    Table Menulayout;
 
     public MainMenu(CPTGame game) {
+
+        stage = new Stage(new FitViewport(1920,1080));
+
+        Gdx.input.setInputProcessor(stage);
+
         this.game = game;
 
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 1920,1080);
 
-        MenuFont = new BitmapFont(Gdx.files.internal("MainMenu/TempusSansITC.fnt"));
+
+
+        //Ui Setup
+        Menulayout = new Table();
+
+        //Button Setup
+        MenuButton HostGame = new MenuButton("Host Game");
+        HostGame.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y) {
+                //
+            }
+        });
+        MenuButton JoinGame = new MenuButton("Join Game", 1.25f);
+        JoinGame.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y) {
+                //
+            }
+        });
+
+
+        MenuButton Settings = new MenuButton( "Settings");
+        Settings.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y) {
+
+            }
+        });
+        MenuButton Quit = new MenuButton( "Quit");
+        Quit.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y) {
+                Gdx.app.exit();
+                System.exit(-1);
+            }
+        });
+
+
+        Menulayout.add(HostGame).width(250).height(100);
+        Menulayout.row();
+        Menulayout.add(JoinGame).width(320).height(150);
+        Menulayout.row();
+        Menulayout.add(Settings).width(250).height(100);
+        Menulayout.row();
+        Menulayout.add(Quit).width(250).height(100);
+        //Menulayout.debug();
+
+
 
         MainMenuMusic = Gdx.audio.newMusic(Gdx.files.internal("Music/Like_the_Flow_of_Time.mp3"));
         MainMenuMusic.setLooping(true);
 
 
         MenuBackground = new Texture(Gdx.files.internal("MainMenu/MainMenu.png"));
+
+
+        Menulayout.setPosition(250, 1080/2 -50);
+        stage.addActor(Menulayout);
+
+
+
+
     }
 
     @Override
@@ -40,23 +108,23 @@ public class MainMenu implements Screen {
     @Override
     public void render(float delta){
         ScreenUtils.clear(0,0,0,1);
-        camera.update();
-        game.batch.setProjectionMatrix(camera.combined);
 
-        game.batch.begin();
-        game.batch.draw(MenuBackground, 0,0,1920,1080);
-        MenuFont.draw(game.batch, "Host Game", 331,496);
-        MenuFont.draw(game.batch, "Join Game", 325,533,72,72,true);
-        MenuFont.draw(game.batch, "Settings", 331,601);
-        MenuFont.draw(game.batch, "Quit", 331, 658);
-        game.batch.end();
+        stage.act(delta);
+
+        stage.getViewport().getCamera().update();
+        stage.getBatch().begin();
+        stage.getBatch().draw(MenuBackground, 0,0,1920,1080);
+        stage.getBatch().end();
+        stage.draw();
+
+
 
 
     }
 
     @Override
     public void resize(int width, int height) {
-
+        stage.getViewport().update(width, height, true);
     }
 
     @Override
