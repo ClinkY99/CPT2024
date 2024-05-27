a45489d211a62419b212297f02b32101c0e11bdb
Clink_Y99 <clinekieran@gmail.com>
2024-05-27 08:57:20 -0400

    Fixed Credits Again

--- core/src/com/mygdx/game/Menus/CreditsScreen.java
+++ core/src/com/mygdx/game/Menus/CreditsScreen.java
@@ -5,8 +5,10 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.mygdx.game.CPTGame;
@@ -16,10 +18,19 @@ public class CreditsScreen implements Screen {
     Texture credits;
     CPTGame game;
     Music music;
+    Stage stage;
+
     public CreditsScreen( CPTGame game, Music music) {
         this.credits = new Texture(Gdx.files.internal("Images/credits.png"));
         this.game =  game;
         this.music = music;
+
+        stage = new Stage(new FitViewport(1920, 1080, new OrthographicCamera()));
+
+        Image creditsImage = new Image(credits);
+        creditsImage.setPosition(0, 0);
+        creditsImage.setSize(1920, 1080);
+        stage.addActor(creditsImage);
 
         music.setLooping(true);
         music.play();
@@ -34,9 +45,9 @@ public class CreditsScreen implements Screen {
     @Override
     public void render(float v) {
         ScreenUtils.clear(0,0,0,1);
-        game.batch.begin();
-        game.batch.draw(credits,0,0,1920 , 1080);
-        game.batch.end();
+
+        stage.draw();
+
         if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
             music.stop();
             game.setScreen(new MainMenu(game,Gdx.audio.newMusic(Gdx.files.internal("Music/Menus/mainMenu.wav"))));
