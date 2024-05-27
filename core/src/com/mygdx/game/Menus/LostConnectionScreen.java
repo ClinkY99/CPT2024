279a64e6bbedf29f6dfbfc6286327158aadf4059
mgorm <mgomangamer@gmail.com>
2024-05-26 22:59:56 -0400

    added connection screen

---
+++ core/src/com/mygdx/game/Menus/LostConnectionScreen.java
@@ -0,0 +1,66 @@
+package com.mygdx.game.Menus;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.mygdx.game.CPTGame;
+import com.mygdx.game.Menus.subMenus.HostGame;
+
+public class LostConnectionScreen implements Screen {
+    Texture credits;
+    CPTGame game;
+    Music music;
+    public LostConnectionScreen( CPTGame game, Music music) {
+        this.credits = new Texture(Gdx.files.internal("Images/connectionScreen.png"));
+        this.game =  game;
+        this.music = music;
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float v) {
+        game.batch.begin();
+        ScreenUtils.clear(0,0,0,1);
+        game.batch.draw(credits,0,0);
+        game.batch.end();
+        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
+            game.setScreen(new MainMenu(game,music));
+        }
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
